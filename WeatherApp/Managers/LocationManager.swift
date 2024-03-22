f8f3d068be9343b38d3065ec3bcf899c81e8cbb2
TheP1ch <beluakov.eugene@bk.ru>
2024-03-22 16:50:51 +0300

    Init Folders

---
+++ WeatherApp/Managers/LocationManager.swift
@@ -0,0 +1,13 @@
+//
+//  CLLocationManager.swift
+//  WeatherApp
+//
+//  Created by Евгений Беляков on 22.03.2024.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationManager: CLLocationManager, CLLocationManagerDelegate {
+    
+}
